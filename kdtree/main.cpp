1d4931c1850560b1bbf7c037574ce52369284d27
Kontaro <moyans@hotmail.com>
2016-05-09 07:27:15 -0300

    creacion base kd-arbol

--- kdtree/main.cpp
+++ kdtree/main.cpp
@@ -4,13 +4,32 @@ using namespace std;
 
 struct nodo{
 
-int x,y,z;
-int depht;
-//aqui deberian ir los hijos
-
-
+    int x,y,z;
+    int depht;
+    //aqui deberian ir los hijos
+    nodo *left ;
+    nodo *right;
+    nodo(){
+        x=0;
+        y=0;
+        z=0;
+        left=NULL;
+        right=NULL;
+    }
 };
 
+
+struct tree{
+
+    nodo *root;
+    nodo *current;
+
+    tree(){
+        root=NULL;
+        current=NULL;
+    }
+
+};
 
 int main()
 {
